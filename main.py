0918260b46a0f1bbeb47641b32054eacd3ccaa45
francois <francois.devrainne@gmail.com>
2021-09-24 11:53:27 +0200

    Change path parameters to query string parameters

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from numpy import int64
 import pandas as pd
 from flask import Flask
-from flask_restful import Api, Resource, abort
+from flask_restful import Api, Resource, reqparse, abort
 
 from constant import END_POINT, DB_PATH
 
 app = Flask(__name__)
 api = Api(app)
 
+arguments = reqparse.RequestParser()
+arguments.add_argument("method", type=str, help="Name of the method is required", required=True)
+arguments.add_argument("column", type=str, help="Name of the column is required", required=True)
+
+
 class GetStats(Resource):
-    def get(self, method, column):
+    def get(self):
+        args = arguments.parse_args()
+        method, column = args["method"], args["column"] 
         df = pd.read_csv(DB_PATH)
         if column not in df.columns:
             abort(404,message=f"The column {column} does not exist in the table")
@@ -25,9 +32,7 @@ class GetStats(Resource):
         return {"result":result}
 
 
-endpoint_params = f"/{END_POINT}/<string:method>/<string:column>"
-api.add_resource(GetStats, endpoint_params)
+api.add_resource(GetStats, f"/{END_POINT}")
 
 if __name__ == "__main__":
     app.run()
-
--- test_api.py
+++ test_api.py
@@ -5,10 +5,11 @@ from constant import BASE_URL, END_POINT
 method = "common"
 column = "MiscFeature"
 
-url = "/".join([BASE_URL, END_POINT, method, column])
-response = requests.get(url)
+url = "/".join([BASE_URL, END_POINT])
+params = {"method":method, "column":column}
+response = requests.get(url, params=params)
 
-print(f"- The call tested is a get request to the following url: \n  {url}\n")
+print(f"- The call tested is a get request to the following url: \n  {url} \n  with the following parameters: {params}\n")
 print(f"- The status code of the API for the call is: {response.status_code} \n  with the following result: {response.json()}")
 
 
